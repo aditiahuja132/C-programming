9e206330aa87b1b78c44b6583a024a338eced92e
Aditi Ahuja <aditiahuja132@gmail.com>
2021-01-07 21:08:29 +0530

    added files

---
+++ daysinmonthsshortway.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int months;
+    printf("Enter the month:");
+    scanf("%d",&months);
+     
+    switch (months)
+    {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 9:
+    case 11:
+    printf("It has 31 days");
+    break;
+    
+    case 2:
+    printf("It has 28 days and 29 days in a leap year");
+    break;
+
+    case 4:
+    case 6:
+    case 10:
+    case 12:
+    printf("It has 30 days");
+    break;
+        default:
+    printf("An year has only 12 months dear:)");
+        break;
+    }
+    return 0;
+}
---
+++ grade%.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    double math, eng, hin, sci, evs, per;
+    printf("Enter the marks of all five subjects out of 100:");
+    scanf("%lf %lf %lf %lf %lf ", &math, &eng, &hin, &sci, &evs);
+    per = ((math+eng+hin+sci+evs)/500)*100;
+    printf("Your percentage is:%.2lf",per);
+
+    if (per >= 90)
+    {
+        printf("\nYour grade is A");
+    }
+
+    else if (per >= 80 && per < 90)
+    {
+        printf("\nYour grade is B");
+    }
+    
+    else if (per >= 70 && per < 80)
+    {
+        printf("\nYour grade is c");
+    }
+
+     else if (per >= 60 && per < 70)
+    {
+        printf("\nYour grade is D");
+    }
+
+     else if (per >= 50 && per < 60)
+    {
+        printf("\nYour grade is E");
+    }
+
+     else if (per < 50)
+     {
+        printf("\nYour Fail, work harder!");
+    }
+    
+    return 0 ;
+}
