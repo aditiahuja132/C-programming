8a0bbd5de43932712c160b258aa796aab84d093e
Aditi Ahuja <aditiahuja132@gmail.com>
2021-03-10 09:28:01 +0530

    html

--- daysinmonthsshortway.c
+++ daysinmonthsshortway.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int months;
     printf("Enter the month:");
-    scanf("%d",&months);
-     
+    scanf("%d", &months);
+
     switch (months)
     {
     case 1:
@@ -13,21 +13,21 @@ int main()
     case 7:
     case 9:
     case 11:
-    printf("It has 31 days");
-    break;
-    
+        printf("It has 31 days");
+        break;
+
     case 2:
-    printf("It has 28 days and 29 days in a leap year");
-    break;
+        printf("It has 28 days and 29 days in a leap year");
+        break;
 
     case 4:
     case 6:
     case 10:
     case 12:
-    printf("It has 30 days");
-    break;
-        default:
-    printf("An year has only 12 months dear:)");
+        printf("It has 30 days");
+        break;
+    default:
+        printf("An year has only 12 months dear:)");
         break;
     }
     return 0;
